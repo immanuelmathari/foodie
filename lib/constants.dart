11563debaee1b2bec916e9075735c6baad71241f
immanuelmathari <immanuelnganga2001@gmail.com>
2024-08-13 06:37:17 +0300

    adding constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,4 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
 
-const double KDefaultPadding = 16.0;
+const double kDefaultPadding = 16.0; // Question for you Immanuel, should this be lower caps or capital
